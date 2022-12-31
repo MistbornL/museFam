9926341bb7fd33d81ca8737183b939f0c5e1b557
MistbornL <lashaloria3@gmail.com>
2022-12-31 16:55:03 +0400

    input component

---
+++ frontend/src/components/InputField.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const InputField = ({ name, register, errors, type = "text" }) => {
+  return (
+    <div className="form-outline col-6 mb-4">
+      <input
+        {...register(name)}
+        id="form"
+        className={`form-control  ${errors[name] ? "is-invalid" : ""}`}
+        placeholder={name + "..."}
+        type={type}
+      />
+      <div className="invalid-feedback">
+        {errors[name] && errors[name].message}
+      </div>
+    </div>
+  );
+};
+
+export default InputField;
