3b77d1f1aee2ecc9c28d17cb7944273670dcbb95
MistbornL <lashaloria3@gmail.com>
2023-01-02 18:24:20 +0400

    tailwind

--- frontend/src/Navigation/Navbar.jsx
+++ frontend/src/Navigation/Navbar.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  const showMenu = () => {
+    setActive(!active);
+  };
   return (
     <div className="md:flex justify-between items-center w-full text-white ">
-      <div className="text-4xl">
+      <div className=" px-4 text-4xl">
         <h1 className="4xl">MuseFam</h1>
       </div>
 
       <nav className="">
+        <div className="absolute right-6 md:hidden top-6 scale-150">
+          <MenuOutlined
+            onClick={showMenu}
+            className="scale-150 cursor-pointer"
+          />
+        </div>
+
         <ul className="md:flex text-2xl gap-8 p-6">
           <li>
             <Link to="/">Home</Link>
