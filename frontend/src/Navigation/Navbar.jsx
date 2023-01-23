dfab10417c13626ac560a3708387da08fc234fd8
MistbornL <lashaloria3@gmail.com>
2023-01-23 20:47:27 +0400

    menu

--- frontend/src/Navigation/Navbar.jsx
+++ frontend/src/Navigation/Navbar.jsx
@@ -5,6 +5,7 @@ import MenuItems from "../components/MenuItems";
 
 export const Navbar = () => {
   const [active, setActive] = useState(false);
+  const token = localStorage.getItem("token");
   const navItems = [
     "home",
     "testimonials",
@@ -19,10 +20,8 @@ export const Navbar = () => {
   };
 
   return (
-    <div className=" w-full text-white flex justify-center p-4 items-center">
-      <h1 className="text-4xl ">MuseFam</h1>
-
-      <nav>
+    <div className="w-full text-white flex justify-center px-4 items-center">
+      <nav className="bg-black rounded-4xl justify-end flex items-center">
         <div className="absolute right-6 md:hidden top-6 scale-150">
           {/* <FontAwesomeIcon
             icon={"fa-xmark"}
@@ -31,19 +30,36 @@ export const Navbar = () => {
           /> */}
         </div>
 
-        <ul className="hidden text-2xl md:flex gap-8 p-6  ">
+        <ul className=" hidden text-2xl md:flex gap-8 p-6  items-center">
+          <Link to={"/"} className="text-4xl ">
+            MuseFam
+          </Link>
           {navItems.map((item, index) => {
             return (
               <li
                 key={index}
                 style={{ borderRadius: "5px" }}
-                className=" hover:scale-150 transition-transform px-3 py-3 "
+                className=" text-gray-light hover:scale-150 transition-transform px-3 py-3 "
               >
-                <Link to={item === "home" ? `/` : `/${item}`}>{item}</Link>
+                <Link to={`/${item}`}>{item}</Link>
               </li>
             );
           })}
         </ul>
+
+        <div className="w-96 flex justify-end text-xl gap-10 px-5">
+          {token ? (
+            <Link onClick={() => localStorage.clear()} to={"/"}>
+              Logout
+            </Link>
+          ) : (
+            <>
+              {" "}
+              <Link to={"/signUp"}>Sign Up</Link>
+              <Link to={"/signIn"}>Sign In</Link>
+            </>
+          )}
+        </div>
 
         <MenuItems showMenu={showMenu} active={active} />
       </nav>
