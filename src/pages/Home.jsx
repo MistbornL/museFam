81cdb012deffcf224c4f30ffb44db7490e1c5fe4
MistbornL <lashaloria3@gmail.com>
2023-01-24 21:07:22 +0400

    transition fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,9 +10,9 @@ export const Home = () => {
 
   return (
     <motion.div
-      initial={{ opacity: 0, y: -100 }}
+      initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: 100 }}
+      exit={{ opacity: 0, y: -100 }}
       transition={{ duration: 0.5, ease: "easeInOut" }}
       className="App"
     >
